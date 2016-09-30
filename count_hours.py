97661ceede54f4aabf7ebcf81aa80a23d298deb7
Julien M'Poy <julien.mpoy@gmail.com>
2016-09-30 13:07:06 +0200

    Create time couting script. Activities are grouped by domain and time is shown in minutes.

---
+++ count_hours.py
@@ -0,0 +1,35 @@
+import sys
+import re
+from itertools import tee
+
+def create_dict(filename):
+    with open(filename) as f:
+        time_regex = re.compile(r"\| \d+")
+        activity_regex = re.compile(r"^(?:Documentation|Test|Développement|Environnement de développement|Environnement de test|Étude de faisabilité)")
+        row_time = None
+        doc_dic = dict()
+
+        for i in range(2):
+            f.readline()
+        for line in f:
+            row_time = time_regex.findall(line)
+            row_activity = activity_regex.findall(line)
+            if row_time != None and len(row_activity):
+                row_activity = row_activity[0]
+                row_time = int(row_time[0][2:])
+                if doc_dic.get(row_activity, None) != None:
+                    doc_dic[row_activity] += row_time
+                else:
+                    doc_dic[row_activity] = row_time
+                print(row_activity, ":", row_time)
+            else:
+                raise NameError('Missing time or activity in the following line: ', line)
+        return doc_dic;
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("falsy call")
+    else:
+        activity_histo = create_dict(sys.argv[1])
+        print(activity_histo)
+        print(sum(activity_histo.values()))
